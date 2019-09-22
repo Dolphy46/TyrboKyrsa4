b74d0c5711326e2237362ddbb027535b81a82608
DeLbFuH <45275322+DeLbFuH@users.noreply.github.com>
2019-09-22 23:24:01 +0300

    Fix with names in Cell

--- TurboKyrsa4/MainClasses/Cell.cs
+++ TurboKyrsa4/MainClasses/Cell.cs
@@ -8,7 +8,7 @@ namespace TurboKyrsa4
 {
     public class Cell //Класс отвечающий за ячейки
     {
-        public bool town;
+        public bool bilding;
 
         public Cell()
         {
@@ -17,17 +17,17 @@ namespace TurboKyrsa4
 
         public Cell(bool town) // Конструктор класса
         {
-            this.town = town;
+            this.bilding = bilding;
         }
 
         public void Set(bool bild) // Функция установки значения нахождения города в ячейке
         {
-            town = bild;
+            bilding = bild;
         }
 
         public bool Get() // Функция получения значения о нахождения города в ячейке
         {
-            return town;
+            return bilding;
         }
 
 
