0e7ff954cae20e07a6075eb6f85e531cfea944bc
DeLbFuH <45275322+DeLbFuH@users.noreply.github.com>
2019-09-26 21:05:25 +0300

    Подправил код

--- TurboKyrsa4/MainClasses/Render.cs
+++ TurboKyrsa4/MainClasses/Render.cs
@@ -18,23 +18,28 @@ namespace TurboKyrsa4
             int x=178, y=75;
             graph = Graphics.FromImage(map);
             Image globalmap = Image.FromFile("MAP V1.jpg");
-            Image town1 = Image.FromFile("Town1.png");
+            Image cell = Image.FromFile("Cell.png");
             Image mountains1 = Image.FromFile("Mountains1.png");
             Image mountains2 = Image.FromFile("Mountains2.png");
             Image mountains3 = Image.FromFile("Mountains3.png");
             Image mountains4 = Image.FromFile("Mountains4.png");
             Image plain = Image.FromFile("Plain.png");
             Image meadow = Image.FromFile("Meadow.png");
+            Image town1 = Image.FromFile("Town1.png");
             graph.DrawImage(globalmap, 1, 1, 1279, 959);
-            bool count = true;
-            getmarks.InfoAboutMap();
+            bool count = true,count1=true;
+            if (count1 == true)
+            {
+                getmarks.InfoAboutMap();
+                count1 = false;
+            }
             bool[,] marks = getmarks.GetinfoAboutMap();
             for (int i = 0; i < 15; i++)
             {
                 for (int i2 = 0; i2 < 7; i2++)
                 {
                     if(marks[i,i2]==true)
-                    graph.DrawImage(town1, x, y, 123, 123);                   
+                    graph.DrawImage(cell, x, y, 123, 123);                   
                     x += 178;
                 }
                 if (count == true)
@@ -60,20 +65,23 @@ namespace TurboKyrsa4
                 {
                     switch (location[i, i2])
                     {
+                        case 0:
+                            graph.DrawImage(plain, x, y, 123, 123);
+                            break;
                         case 1:
-                            graph.DrawImage(town1, x, y, 123, 123);
+                            graph.DrawImage(meadow, x, y, 123, 123);
                             break;
                         case 2:
-                            graph.DrawImage(town1, x, y, 123, 123);
+                            graph.DrawImage(mountains1, x, y, 123, 123);
                             break;
                         case 3:
-                            graph.DrawImage(town1, x, y, 123, 123);
+                            graph.DrawImage(mountains2, x, y, 123, 123);
                             break;
                         case 4:
-                            graph.DrawImage(town1, x, y, 123, 123);
+                            graph.DrawImage(mountains3, x, y, 123, 123);
                             break;
                         case 5:
-                            graph.DrawImage(town1, x, y, 123, 123);
+                            graph.DrawImage(mountains4, x, y, 123, 123);
                             break;
                         case 6:
                             graph.DrawImage(town1, x, y, 123, 123);
